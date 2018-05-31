103148d6010bbed7592573e2b23d6e8278a753dc
Remzi <remziz44@live.com>
2018-05-30 23:40:01 -0400

    Set up Spring Framework using Spring Boot

---
+++ src/main/java/gg/levelplusone/lpoapp/LpoappApplication.java
@@ -0,0 +1,12 @@
+package gg.levelplusone.lpoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpoappApplication.class, args);
+	}
+}
