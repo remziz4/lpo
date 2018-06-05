d33725c40bf763da4b8f4855b6eb049a8567bfa1
Remzi <remziz44@live.com>
2018-06-04 22:50:30 -0400

    Post Request binding via postman, still not via form

--- src/main/java/gg/levelplusone/lpoapp/controller/AuthorizationController.java
+++ src/main/java/gg/levelplusone/lpoapp/controller/AuthorizationController.java
@@ -2,7 +2,9 @@ package gg.levelplusone.lpoapp.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +19,16 @@ public class AuthorizationController {
 
 
 	@RequestMapping(value = "/registerUser", method = RequestMethod.POST)
-	public ResponseEntity<String> registerUserAccount(@Valid AuthorizationUserVO authUser) {
-		
+	public ResponseEntity<String> registerUserAccount(@RequestBody AuthorizationUserVO authUser) {
+		System.out.println("Entered AuthorizationController.registerUserAccount");
+		System.out.println(authUser);
 		User registered = RegisterUserService.registerUser(authUser);
 		
 		if(registered == null) {
-			return ResponseEntity.ok("success");
+			return new ResponseEntity<String>("success", HttpStatus.OK);
 		}
 		
-		return ResponseEntity.ok("success");
+		return new ResponseEntity<String>("success", HttpStatus.OK);
 		
 	}
 	
